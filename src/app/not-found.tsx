8c4fe59d12d198c16ec07bafe12ff78d1d5e3e18
Alessandro Figo <80041061+mkuwdev@users.noreply.github.com>
2024-02-28 15:28:38 +0700

    ✨ new: /latest redirect, 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,7 +10,7 @@ export default function NotFound() {
       <div className="h-screen w-full flex justify-center items-center font-mono">
         <div className="flex flex-col items-center space-y-4">
           <div className="text-4xl lg:text-7xl font-bold text-blue">404 - Not Found</div>
-          <div className="text-sm lg:text-xl">Sorry, the page you're looking for does not exist.</div>
+          <div className="text-sm lg:text-xl">{`Sorry, the page you're looking for does not exist.`}</div>
           <Link className="!mt-8 uppercase text-xs hover:cursor-pointer underline md:no-underline hover:underline underline-offset-8" href="/">
             Take me back home
           </Link>
